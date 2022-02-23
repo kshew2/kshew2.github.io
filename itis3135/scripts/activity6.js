6c4c3427c1e5bf1af50c4b5a83d5c364f83705a4
kshew2 <kshew2@uncc.edu>
2022-02-22 21:36:37 -0500

    polygon activity

---
+++ itis3135/scripts/activity6.js
@@ -0,0 +1,76 @@
+/*function getNumSides()
+{
+let input = promt("The Khaki Salamander asks you to enter the number 1-10 to reveal your chosen polygon");
+random entry does not work
+}*/
+function getNumSides()
+{
+    var input = prompt("The Khaki Salamander promts you to enter a number 1-10 to get your shape");
+    validateNum(input);
+}
+
+function validateNum(_input)
+{
+    if(isNaN(_input)){
+        alert("Please enter a number.");
+        getNumSides();
+    }
+    else
+    {
+        guess = Math.trunc(_input);
+        if(guess < 1 || guess > 10)
+        {
+            alert("Your number must be between 1-10");
+            getNumSides();
+        }
+        else
+        {
+            getShape(guess);
+        }
+    }
+}
+/*returns the number as a shape*/
+function getShape(sides)
+{
+   if(guess == 1)
+   {
+       alert("Your shape is henagon");
+   } 
+   else if(guess == 2)
+   {
+       alert("Your shape is digon");
+   }
+   else if(guess == 3)
+   {
+       alert("Your shape is trigon");
+   }
+   else if(guess == 4)
+   {
+       alert("Your shape is tetragon");
+   }
+   else if(guess == 5)
+   {
+       alert("Your shape is pentagon");
+   }
+   else if(guess == 6)
+   {
+       alert("Your shape is hexagon");
+   }
+   else if(guess == 7)
+   {
+       alert("Your shape is septagon");
+   }
+   else if(guess == 8)
+   {
+       alert("Your shape is octagon");
+   }
+   else if(guess == 9)
+   {
+       alert("Your shape is nonagon");
+   }
+   else if(guess == 10)
+   {
+       alert("Your shape is decagon")
+   }
+ 
+}
---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,44 @@
+function greeting()
+{
+    let name = document.getElementById("name").value;
+    let feeling = document.getElementById("feeling").value;
+    let message = "The Khaki Salamander says hi, " + name + "! We're glad you are doing " + feeling + "!";
+    document.getElementById("greetingMessage").innerHTML = message;
+}
+function randomFact1()
+{
+    let factList = new Array(
+        "Salamanders are generally 4 to 6 inches long."
+    );
+
+    let randNum = Math.floor(Math.random() * factList.length);   
+    document.getElementById("randomFact1").innerHTML = factList[randNum];
+}
+function randomFact2()
+{
+    let factList = new Array(
+        "Many are camouflaged, whereas others are boldly patterned or brightly coloured."
+    );
+
+    let randNum = Math.floor(Math.random() * factList.length);   
+    document.getElementById("randomFact2").innerHTML = factList[randNum];
+}
+function randomFact3()
+{
+    let factList = new Array(
+        "The largest species is the Chinese Giant Salamander."
+    );
+
+    let randNum = Math.floor(Math.random() * factList.length);   
+    document.getElementById("randomFact3").innerHTML = factList[randNum];
+}
+function randomFact4()
+{
+    let factList = new Array(
+        "Amphibians in the family of over 740 species moist skinned animals with tails and feet."
+    );
+
+    let randNum = Math.floor(Math.random() * factList.length);   
+    document.getElementById("randomFact4").innerHTML = factList[randNum];
+}
+
