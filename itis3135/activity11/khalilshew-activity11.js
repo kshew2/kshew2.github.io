41682bc71658cfb54673099f7b5c5f8ffa562f72
kshew2 <kshew2@uncc.edu>
2022-05-07 17:18:10 -0400

    Finished site

---
+++ itis3135/activity11/khalilshew-activity11.js
@@ -0,0 +1,10 @@
+//<!-- jQuery call to the accordion() method. -->
+$(document).ready(function () {
+    $('#accordion').accordion({
+
+        active: false,
+        collapsible: true,
+        heightStyle: 'panel'
+
+    });
+});
