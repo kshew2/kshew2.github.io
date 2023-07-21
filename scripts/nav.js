8c546ae3a0fa498c5ffc953bcd9d29c6cffbb9bd
kshew2 <kshew2@uncc.edu>
2023-07-21 12:10:58 -0400

    push

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,22 @@
 function navFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("myTopNav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
   }
+
+  function navFunction2() {
+    var x = document.getElementById("myBottomNav");
+    if (x.className === "footer") {
+      x.className += " responsive";
+    } else {
+      x.className = "footer";
+    }
+  
+} 
+
+function bars() {
+    document.getElementById("github").innerHTML ="Github Repo";
+}
